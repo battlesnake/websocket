65213ce44a4fcdd20c98631fe8bf08a4b5355d0a
Mark K Cowan <mark@battlesnake.co.uk>
2017-01-15 12:49:29 +0000

    send() now returns a promise, "json" can be specified for automatic encoding/decoding

--- index.js
+++ index.js
@@ -2,24 +2,69 @@ const browser = typeof window !== 'undefined';
 const Impl = browser ? window.WebSocket : require('w' + 's');
 const EventEmitter = require('eventemitter');
 
-Websocket.prototype = new EventEmitter();
-
 let socket_id = 0;
+
+/* Used for detecting wrap-existing-websocket call */
+const wrap = {};
+
+/* Wrap an existing websocket, e.g. from a websocket server */
+Websocket.wrap = (...args) => new Websocket(wrap, ...args);
+
+Websocket.prototype = new EventEmitter();
 
 /* "s" intentionally lowercase */
 function Websocket(...args) {
 	EventEmitter.call(this);
+
 	const id = ++socket_id;
 
-	const ws = new Impl(...args);
+	const json = args[0] === 'json';
+	if (json) {
+		args.shift();
+	}
+
+	const ws = args[0] === wrap ? args[1] : new Impl(...args);
+
+	const receive = data => {
+		if (json) {
+			try {
+				this.emit('message', JSON.parse(data));
+			} catch (err) {
+				this.emit('error', new Error('Invalid JSON received'));
+			}
+		} else {
+			this.emit('message', data);
+		}
+	};
+
+	const send = data => new Promise((res, rej) => {
+		if (json) {
+			data = JSON.stringify(data);
+		}
+		try {
+			if (browser) {
+				ws.send(data);
+				res(data);
+			} else {
+				ws.send(data, err => {
+					if (err) {
+						rej(err);
+					}
+					res(err);
+				});
+			}
+		} catch (err) {
+			rej(err);
+		}
+	});
 
 	if (browser) {
-		ws.onmessage = event => this.emit('message', event.data);
-		ws.onerror = event => this.emit('error', ...args); /* TODO */
+		ws.onmessage = event => receive(event.data);
+		ws.onerror = event => this.emit('error', event); /* TODO: error message */
 		ws.onopen = event => this.emit('open');
 		ws.onclose = event => this.emit('close');
 	} else {
-		ws.onmessage = event => this.emit('message', event.data);
+		ws.onmessage = event => receive(event.data);
 		ws.onerror = err => this.emit('error', err);
 		ws.onopen = () => this.emit('open');
 		ws.onclose = () => this.emit('close');
@@ -35,15 +80,16 @@ function Websocket(...args) {
 		this.on('close', () => log('close'));
 	}
 
-	this.send = (data, cb) => {
-		try {
-			ws.send(data);
-			this.emit('send', data);
-		} catch (err) {
-			return cb && cb(err), undefined;
-		}
-		return cb && cb(), undefined;
-	};
+	this.send = data => send(data)
+		.then(
+			() => {
+				this.emit('send', data);
+			},
+			err => {
+				this.emit('error', err);
+				return Promise.reject(err);
+			});
+
 	this.close = (...args) => ws.close(...args);
 }
 
